f651ada8f73784083a5b7d4c5b581340d185437c
Roberto <roberto.starnini@gmail.com>
2015-09-28 11:07:25 +0200

    Advanced filter criteria in document views

--- Products/CMFPlomino/PlominoView.py
+++ Products/CMFPlomino/PlominoView.py
@@ -28,7 +28,7 @@ from Acquisition import aq_inner
 from Products.Archetypes.atapi import *
 from Products.ATContentTypes.content.folder import ATFolder
 from zope.interface import implements
-from Products.CMFPlomino.PlominoUtils import translate
+from Products.CMFPlomino.PlominoUtils import translate, asUnicode, asList
 
 # Plone
 try:
@@ -48,6 +48,12 @@ from Products.CMFPlomino.config import *
 from Products.CMFPlomino.browser import PlominoMessageFactory as _
 from validator import isValidPlominoId
 import interfaces
+
+# queryDocuments
+from dateutil.parser import parse as parse_date
+from Products.PluginIndexes.DateIndex.DateIndex import DateIndex
+from Products.CMFPlomino.index import PlominoIndex
+
 
 import logging
 logger = logging.getLogger('Plomino')
@@ -863,6 +869,137 @@ class PlominoView(ATFolder):
             else:
                 key = ''
         return key
+
+    # queryDocuments
+    # Return all the documents matching the view and the custom filter criteria.
+    security.declarePublic('queryDocuments')    
+    def queryDocuments(self, start=1, limit=None, only_allowed=True,
+        getObject=True, fulltext_query=None, sortindex=None, reverse=None,
+        query_request={}):
+        """
+        Return all the documents matching the view and the custom filter criteria.
+        """
+        index = self.getParentDatabase().getIndex()
+        if not sortindex:
+            sortindex = self.getSortColumn()
+            if sortindex=='':
+                sortindex=None
+            else:
+                sortindex=self.getIndexKey(sortindex)
+        if not reverse:
+            reverse = self.getReverseSorting()
+        query = {'PlominoViewFormula_'+self.getViewName() : True}
+
+        total = len(index.dbsearch(query, only_allowed=only_allowed))
+
+        query.update(query_request)
+
+        if fulltext_query:
+            query['SearchableText'] = fulltext_query
+        results=index.dbsearch(
+            query,
+            sortindex=sortindex,
+            reverse=reverse,
+            only_allowed=only_allowed)
+        if limit:
+            results = batch(
+                    results,
+                    pagesize=limit,
+                    pagenumber=int(start/limit)+1)
+        if getObject:
+            return [r.getObject() for r in results], total
+        else:
+            return results, total
+
+    security.declarePublic('search_json')
+    def search_json(self, REQUEST=None):
+        """ Returns a JSON representation of view filtered data
+        """
+        data = []
+        categorized = self.getCategorized()
+        start = 1
+        search = None
+        sort_index = None
+        reverse = 1
+
+        if REQUEST:
+            start = int(REQUEST.get('iDisplayStart', 1))
+
+            limit = REQUEST.get('iDisplayLength')
+            limit = (limit and int(limit))
+            # In case limit == -1 we want it to be None
+            if limit < 1:
+                limit = None
+
+            search = REQUEST.get('sSearch', '').lower()
+            if search:
+                search = " ".join([term+'*' for term in search.split(' ')])
+            sort_column = REQUEST.get('iSortCol_0')
+            if sort_column:
+                sort_index = self.getIndexKey(self.getColumns()[int(sort_column)-1].id)
+            reverse = REQUEST.get('sSortDir_0') or 'asc'
+            if reverse=='desc':
+                reverse = 0
+            if reverse=='asc':
+                reverse = 1
+
+        query_request = json.loads(REQUEST.get('query') or '{}')
+        # Some fields might express a date
+        # We try to convert those strings to datetime
+        #indexes = self.aq_parent.aq_base.plomino_index.Indexes
+        indexes = self.getParentDatabase().getIndex().Indexes
+        for key, value in query_request.iteritems():
+            if key in indexes:
+                index = indexes[key]
+                # This is lame: we should check if it quacks, not
+                # if it's a duck!
+                # XXX Use a more robust method to tell apart
+                # date indexes from non-dates
+
+                # I'd use a solution like this one: http://getpython3.com/diveintopython3/examples/customserializer.py
+                if isinstance(index, DateIndex):
+                    # convert value(s) to date(s)
+                    if isinstance(value, basestring):
+                        query_request[key] = parse_date(value)
+                    else:
+                        if isinstance(value['query'], basestring):
+                            value['query'] = parse_date(value['query'])
+                        else:
+                            query_request[key]['query'] = [parse_date(v) for v in value['query']]
+
+        results, total = self.queryDocuments(start=1,
+                                       limit=None,
+                                       getObject=False,
+                                       fulltext_query=search,
+                                       sortindex=sort_index,
+                                       reverse=reverse,
+                                       query_request=query_request)
+        
+        if limit:
+            results = batch(
+                    results,
+                    pagesize=limit,
+                    pagenumber=int(start/limit)+1)
+        display_total = len(results)
+        
+        columnids = [col.id for col in self.getColumns() if not getattr(col, 'HiddenColumn', False)]
+        for b in results:
+            row = [b.getPath().split('/')[-1]]
+            for colid in columnids:
+                v = getattr(b, self.getIndexKey(colid), '')
+                if isinstance(v, list):
+                    v = [asUnicode(e).encode('utf-8').replace('\r', '') for e in v]
+                else:
+                    v = asUnicode(v).encode('utf-8').replace('\r', '')
+                row.append(v or '&nbsp;')
+            if categorized:
+                for cat in asList(row[1]):
+                    entry = [c for c in row]
+                    entry[1] = cat
+                    data.append(entry)
+            else:
+                data.append(row)
+        return json.dumps({ 'iTotalRecords': total, 'iTotalDisplayRecords': display_total, 'aaData': data })    
 
     def Column_vocabulary(self):
         return [''] + [c.id for c in self.getColumns()]
