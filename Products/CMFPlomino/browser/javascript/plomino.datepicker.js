f6980516fb8ca3d9f65deeb9f14894880ddf44c1
Roberto <roberto.starnini@gmail.com>
2015-12-24 17:31:35 +0100

    add opendialog event

--- Products/CMFPlomino/browser/javascript/plomino.datepicker.js
+++ Products/CMFPlomino/browser/javascript/plomino.datepicker.js
@@ -4,13 +4,14 @@
         var value = $(this).val();
         var options = $(this).data('datepickerOptions');
         if(typeof(options)!='object') options = {};
-        if ($(this).data('datepickerDateformat')&&!('dateFormat' in options)) {
-            /* in case you use unusual date format not easily convertible
-               between jQuery dateFormat and python datetime format */
+        if ( $(this).data('datepickerDateformat') ) {
             options['dateFormat'] = $(this).data('datepickerDateformat');
-        };
+        };        
         if ( $(this).data('datepickerMindate') ) {
             options['minDate'] = $(this).data('datepickerMindate');
+        };
+        if ( $(this).data('datepickerMaxdate') ) {
+            options['maxDate'] = $(this).data('datepickerMaxdate');
         };
         $(this).datepicker( "option", options );
         $(this).datepicker( "setDate", value );
@@ -28,3 +29,28 @@
     });
 
 })(jQuery);
+
+
+$(document).ready(function() {
+    // datepicker widget
+    $(".dateccccccc").datepicker().each(function(){
+        var value = $(this).val();
+        eval("var options = "  + $(this).data('datepickerOptions')+" ||{}");
+        if ($(this).data('datepickerDateformat')&&!('dateFormat' in options)) {
+            /* in case you use unusual date format not easily convertible
+               between jQuery dateFormat and python datetime format */
+            options['dateFormat'] = $(this).data('datepickerDateformat');
+        };
+        if ( $(this).data('datepickerMindate') ) {
+            options['minDate'] = $(this).data('datepickerMindate');
+        };
+        if ( $(this).data('datepickerMaxdate') ) {
+            options['maxDate'] = $(this).data('datepickerMaxdate');
+        };
+        $(this).datepicker( "option", options );
+        $(this).datepicker( "setDate", value );
+        $('#btn_' + this.id).click(function() {
+            $(this).datepicker('show');
+        });
+    });
+});
